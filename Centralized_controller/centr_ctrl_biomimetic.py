ad0b24718013e5b20a84acc7eefa590a70baf245
Matthias2807 <matthias.pex@telenet.be>
2024-03-21 08:53:27 +0100

    .py files run through bash scripts now

--- Centralized_controller/centr_ctrl_biomimetic.py
+++ Centralized_controller/centr_ctrl_biomimetic.py
@@ -1,9 +1,79 @@
 import argparse
 
+# load arguments
+parser = argparse.ArgumentParser(description='Centralized controller')
+
+# main arguments
+parser.add_argument('interface', type=str, help='either VSC or HPC, activates certain parts of code')
+parser.add_argument('arm_setup', type=str, help='delimited string containing: number of segments for each of 5 arms')
+parser.add_argument('reward_type', type=str, choices = ['distance', 'target', 'light'], help='choose from distance, target or light')
+
+parser.add_argument('es_popsize', type=int, help='the number of parallel policy params to evaluate every generation')
+parser.add_argument('num_generations', type=int, help='the number generations for es to run')
+
+parser.add_argument('sensor_selection', type=str, help='delimited string containing: selected sensors')
+parser.add_argument('layers', type=str, help='delimited string containing: neural network dense layers architecture')
+parser.add_argument('damage', type=str, help='whether a damage simulation is run')
+
+parser.add_argument('project', type=str, help='project title wandb')
+parser.add_argument('notes', type=str, help='notes to pass on to wandb')
+
+# optional arguments with default values
+parser.add_argument('--joint_control', choices = ['position', 'torque'], default = 'position', help='whether nn controls positions or torques of joints')
+parser.add_argument('--sand_ground_color', type=str, default = 'False', help='whether a damage simulation is run')
+parser.add_argument('--num_physics_steps_per_control_step', type=int, default = 10)
+parser.add_argument('--simulation_time', type=int, default = 5, help='number of seconds before an episode finishes')
+parser.add_argument('--joint_randomization_noise_scale', type=float, default = 0.0, help='whether joints are in a random position when initialized')
+parser.add_argument('--light_perlin_noise_scale', type=int, default = 4, help='if 0: no light gradients; choose integer factor of 200')
+parser.add_argument('--target_distance', type=float, default = 3.0, help='radius where target is located on')
+parser.add_argument('--arm_setup_damage', type=str, help='delimited string containing damaged configuration: only used if damage=True')
+
+args = parser.parse_args()
+
+# readout the main arguments
+interface = args.interface
+arm_setup = [int(item) for item in args.arm_setup.split(',')]
+reward_type = args.reward_type
+es_popsize = args.es_popsize
+num_generations = args.num_generations
+sensor_selection = tuple([str(item) for item in args.sensor_selection.split(',')])
+layers = [int(item) for item in args.layers.split(',')]
+damage = args.damage=='True'
+project = args.project
+notes = args.notes
+
+# readout the optional arguments or provide default value if not yet provided
+joint_control = args.joint_control
+sand_ground_color = args.sand_ground_color=='True'
+num_physics_steps_per_control_step = args.num_physics_steps_per_control_step
+simulation_time = args.simulation_time
+joint_randomization_noise_scale = args.joint_randomization_noise_scale
+light_perlin_noise_scale = args.light_perlin_noise_scale
+target_distance = args.target_distance
+
+if args.arm_setup_damage:
+    arm_setup_damage = [int(item) for item in args.arm_setup_damage.split(',')]
+else:
+    arm_setup_damage = arm_setup
+
+
 import sys
 print("sys.executable: ", sys.executable)
-# sys.path.insert(0,'C:\\Users\\Matthias\\OneDrive - UGent\\Documents\\DOCUMENTEN\\3. Thesis\\BSC\\')
+if interface == 'VSC':
+    print('sys path insert enabled for VSC')
+    sys.path.insert(0,'C:\\Users\\Matthias\\OneDrive - UGent\\Documents\\DOCUMENTEN\\3. Thesis\\BSC\\')
 
+
+print(f"""
+      damage = {damage}
+      arm_setup = {arm_setup}
+      sensor_selection = {sensor_selection}
+      layers = {layers}
+      layers.append(5) = {layers + [5]}
+      target_distance = {target_distance}
+      notes = {notes}
+      sand_ground_color = {sand_ground_color}
+      """)
 
 import numpy as np
 import jax
@@ -20,7 +90,7 @@ import matplotlib.pyplot as plt
 import wandb
 # query notes to save with the wandb run
 # notes = input("Notes to pass on to wandb initialisation: ")
-notes = "test_trial_batch_job"
+
 
 import moviepy
 import imageio
@@ -199,20 +269,20 @@ rollout = jax.jit(rollout, static_argnames=("total_num_control_timesteps", "NUM_
 
 
 
-arm_setup = [5,0,5,0,0] # 2 arms with 5 segments
+# arm_setup = [5,0,5,0,0] # 2 arms with 5 segments
 dofs = 2*sum(arm_setup)
 print("Number of actuator outputs: dofs = ", dofs)
 
 
 
-reward_type = "distance" # choose "distance", "target", "light"
-joint_control = "position" # choose "position" or "torque"
-sand_ground_color = False # choose True or False, False allows for seeing movement with respect to grid lines.
-num_physics_steps_per_control_step=10 
-simulation_time=5 # [seconds]
-joint_randomization_noise_scale=0.0
-light_perlin_noise_scale=4 # default: take 0 for no noise, take max of 200, take integer factor of 200
-# If this value is > 0 then we will add randomly sampled noise to the initial joint positions and velocities
+# reward_type = "distance" # choose "distance", "target", "light"
+# joint_control = "position" # choose "position" or "torque"
+# sand_ground_color = False # choose True or False, False allows for seeing movement with respect to grid lines.
+# num_physics_steps_per_control_step=10 
+# simulation_time=5 # [seconds]
+# joint_randomization_noise_scale=0.0
+# light_perlin_noise_scale=4 # default: take 0 for no noise, take max of 200, take integer factor of 200
+# # If this value is > 0 then we will add randomly sampled noise to the initial joint positions and velocities
 
 
 # specifying morphology
@@ -249,7 +319,7 @@ if reward_type == "distance":
 elif reward_type == "target":
     environment_configuration = BrittleStarDirectedLocomotionEnvironmentConfiguration(
         # Distance to put our target at (targets are spawned on a circle around the starting location with this given radius).
-        target_distance=3.0,
+        target_distance=target_distance,
         joint_randomization_noise_scale=joint_randomization_noise_scale,
         render_mode="rgb_array",
         simulation_time=simulation_time,
@@ -289,9 +359,9 @@ total_num_control_timesteps = environment_configuration.total_num_control_steps
 ##########################################
 # instantiating the vectorized environment
 ##########################################
-es_popsize = 50
+# es_popsize = 400
 NUM_MJX_ENVIRONMENTS = es_popsize # best to get close to number of CUDA cores available
-num_generations = 50
+# num_generations = 50
 
 mjx_vectorized_env = create_environment(
                 morphology_specification=morphology_specification,
@@ -313,7 +383,7 @@ mjx_vectorized_state = mjx_vectorized_reset(rng=mjx_vectorized_env_rng)
 
 sensors = [key for key in mjx_vectorized_state.observations.keys()]
 sensors_with_dim = {s: d.shape for s, d in mjx_vectorized_state.observations.items()}
-sensor_selection = ('joint_position', 'joint_actuator_force', 'segment_contact')
+# sensor_selection = ('joint_position', 'joint_actuator_force', 'segment_contact')
 sensor_selection_dim = sum([mjx_vectorized_state.observations[sensor].shape[-1] for sensor in sensor_selection])
 
 print(f"""
@@ -344,7 +414,13 @@ print(f"""
 # Instantiating the neural network
 ###################################
 
-features = [128,128, nn_output_dim]
+# features = [128,128, nn_output_dim]
+features = layers + [nn_output_dim]
+print(f"""
+      features = {features}
+      layers = {layers}
+      type(layers) = {type(layers)}
+      """)
 model = ExplicitMLP(features = features, joint_control = joint_control)
 
 # initialising the parameters of the model
@@ -440,7 +516,7 @@ policy_params_to_render = []
 # start a new wandb run to track this script
 wandb.init(
     # set the wandb project where this run will be logged
-    project = "Switch to VSC",
+    project = project,
     # set run name
     name = f"{reward_type} {joint_control} arms {arm_setup} popsize {es_popsize} {notes}",
     # put notes that are queried by the user
@@ -461,7 +537,7 @@ wandb.init(
 start_time = time.time()
 for gen in range(num_generations):
     # track runtimes
-    if gen%1 == 0:
+    if gen%10 == 0:
         print('generation: ', gen, '\ntime since start: ', time.time()-start_time)
         
     
@@ -498,13 +574,7 @@ for gen in range(num_generations):
     # fitness should be an array with population size as len (e.g. 100)
     fit_re = fit_shaper.apply(policy_params, fitness)
 
-    if gen == 10:
-        print(f"""
-              fitness: {fitness}
-              fitness reshaped: {fit_re}
-        """)
-        break
-    
+   
     # log metrics to wandb
     wandb.log({"mean reward": jnp.mean(total_reward),
                "max reward":jnp.max(total_reward),
@@ -615,8 +685,8 @@ while not jnp.any(mjx_vectorized_state.terminated | mjx_vectorized_state.truncat
                 )
             )
 
-# show_video(images=mjx_frames, fps = fps)
-# create_video(frames=mjx_frames, framerate=fps, out_path=f"{reward_type} {joint_control} arms {arm_setup} popsize {es_popsize} {notes}.mp4")
+show_video(images=mjx_frames, fps = fps)
+create_video(frames=mjx_frames, framerate=fps, out_path=f"Videos\{reward_type} {joint_control} arms {arm_setup} popsize {es_popsize} {notes}.mp4")
 
 
 
@@ -654,82 +724,87 @@ wandb.finish()
 # Damage: Applying trained policy to a damaged morphology
 #########################################################
 
-arm_setup_damage = [5,0,0,0,0]
-check_damage(arm_setup = arm_setup, arm_setup_damage = arm_setup_damage)
-# specifying morphology
-morphology_specification_damage = default_brittle_star_morphology_specification(
-        num_arms=len(arm_setup_damage), num_segments_per_arm=arm_setup_damage, use_p_control=(joint_control == 'position'), use_torque_control=(joint_control == 'torque')
-        )
-morphology_damage = create_morphology(morphology_specification=morphology_specification_damage)
-visualize_mjcf(mjcf=morphology_damage)
 
-mjx_vectorized_env = create_environment(
-                morphology_specification=morphology_specification_damage,
-                arena_configuration=arena_configuration,
-                environment_configuration=environment_configuration,
-                backend="MJX"
-                )
+# arm_setup_damage = [5,0,0,0,0]
 
-
-
-NUM_MJX_ENVIRONMENTS_render = policy_params_to_render.shape[0]
-
-rng, mjx_vectorized_env_rng = jax.random.split(rng, 2)
-mjx_vectorized_env_rng = jnp.array(jax.random.split(mjx_vectorized_env_rng, NUM_MJX_ENVIRONMENTS_render))
-
-# vectorizing the functionalities
-mjx_vectorized_step = jax.jit(jax.vmap(mjx_vectorized_env.step))
-mjx_vectorized_reset = jax.jit(jax.vmap(mjx_vectorized_env.reset))
-
-mjx_vectorized_state = mjx_vectorized_reset(rng=mjx_vectorized_env_rng)
-
-
-
-mjx_frames_damage = []
-
-# joint sensor observations order seg1ip, seg1oop, seg2ip, seg2oop, ...
-joint_angles_ip = []
-joint_angles_oop = []
-
-i = 0
-while not jnp.any(mjx_vectorized_state.terminated | mjx_vectorized_state.truncated):
-    if i%10 == 0:
-        print(i)
-    i += 1
-    
-    sensory_input = jnp.concatenate(
-        [mjx_vectorized_state.observations[label] for label in sensor_selection],
-        axis = 1
-    )
-
-    sensory_input_pad = pad_sensory_input(sensory_input, arm_setup, arm_setup_damage, sensor_selection)
-    
-    joint_angles_ip_t = []
-    joint_angles_oop_t = []
-    j = 0
-    for n in arm_setup:
-        if n != 0:
-            joint_angles_ip_t.append(mjx_vectorized_state.observations["joint_position"][0][j*2*n:(j+1)*2*n:2])
-            joint_angles_oop_t.append(mjx_vectorized_state.observations["joint_position"][0][j*2*n+1:(j+1)*2*n+1:2])
-            j += 1
-
-    joint_angles_ip.append(joint_angles_ip_t)
-    joint_angles_oop.append(joint_angles_oop_t)
-
-    action = vectorized_model_apply(policy_params_solution_shaped, sensory_input_pad)
-    action_selection = select_actuator_output(action, arm_setup, arm_setup_damage)
-    
-    mjx_vectorized_state = mjx_vectorized_step(state=mjx_vectorized_state, action=action_selection)
-
-    
-    mjx_frames_damage.append(
-            post_render(
-                mjx_vectorized_env.render(state=mjx_vectorized_state),
-                mjx_vectorized_env.environment_configuration
-                )
+if damage:
+    check_damage(arm_setup = arm_setup, arm_setup_damage = arm_setup_damage)
+    # specifying morphology
+    morphology_specification_damage = default_brittle_star_morphology_specification(
+            num_arms=len(arm_setup_damage), num_segments_per_arm=arm_setup_damage, use_p_control=(joint_control == 'position'), use_torque_control=(joint_control == 'torque')
             )
+    morphology_damage = create_morphology(morphology_specification=morphology_specification_damage)
+    visualize_mjcf(mjcf=morphology_damage)
 
-# show_video(images=mjx_frames_damage, fps=fps)
-# create_video(frames=mjx_frames_damage, framerate=fps, out_path=f"{reward_type} {joint_control} arms {arm_setup} popsize {es_popsize} {notes} DAMAGE.mp4")
+    mjx_vectorized_env = create_environment(
+                    morphology_specification=morphology_specification_damage,
+                    arena_configuration=arena_configuration,
+                    environment_configuration=environment_configuration,
+                    backend="MJX"
+                    )
 
+
+
+    NUM_MJX_ENVIRONMENTS_render = policy_params_to_render.shape[0]
+
+    rng, mjx_vectorized_env_rng = jax.random.split(rng, 2)
+    mjx_vectorized_env_rng = jnp.array(jax.random.split(mjx_vectorized_env_rng, NUM_MJX_ENVIRONMENTS_render))
+
+    # vectorizing the functionalities
+    mjx_vectorized_step = jax.jit(jax.vmap(mjx_vectorized_env.step))
+    mjx_vectorized_reset = jax.jit(jax.vmap(mjx_vectorized_env.reset))
+
+    mjx_vectorized_state = mjx_vectorized_reset(rng=mjx_vectorized_env_rng)
+
+
+
+    mjx_frames_damage = []
+
+    # joint sensor observations order seg1ip, seg1oop, seg2ip, seg2oop, ...
+    joint_angles_ip = []
+    joint_angles_oop = []
+
+    i = 0
+    while not jnp.any(mjx_vectorized_state.terminated | mjx_vectorized_state.truncated):
+        if i%10 == 0:
+            print(i)
+        i += 1
+        
+        sensory_input = jnp.concatenate(
+            [mjx_vectorized_state.observations[label] for label in sensor_selection],
+            axis = 1
+        )
+
+        sensory_input_pad = pad_sensory_input(sensory_input, arm_setup, arm_setup_damage, sensor_selection)
+        
+        joint_angles_ip_t = []
+        joint_angles_oop_t = []
+        j = 0
+        for n in arm_setup:
+            if n != 0:
+                joint_angles_ip_t.append(mjx_vectorized_state.observations["joint_position"][0][j*2*n:(j+1)*2*n:2])
+                joint_angles_oop_t.append(mjx_vectorized_state.observations["joint_position"][0][j*2*n+1:(j+1)*2*n+1:2])
+                j += 1
+
+        joint_angles_ip.append(joint_angles_ip_t)
+        joint_angles_oop.append(joint_angles_oop_t)
+
+        action = vectorized_model_apply(policy_params_solution_shaped, sensory_input_pad)
+        action_selection = select_actuator_output(action, arm_setup, arm_setup_damage)
+        
+        mjx_vectorized_state = mjx_vectorized_step(state=mjx_vectorized_state, action=action_selection)
+
+        
+        mjx_frames_damage.append(
+                post_render(
+                    mjx_vectorized_env.render(state=mjx_vectorized_state),
+                    mjx_vectorized_env.environment_configuration
+                    )
+                )
+
+    show_video(images=mjx_frames_damage, fps=fps)
+    create_video(frames=mjx_frames_damage, framerate=fps, out_path=f"Videos\{reward_type} {joint_control} arms {arm_setup} popsize {es_popsize} {notes} DAMAGE.mp4")
+
+else:
+    print('no damage simulation has been run')
 
---
+++ bash_script_VSC.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# change to the root directory (BSC)
+cd 'C:\Users\Matthias\OneDrive - UGent\Documents\DOCUMENTEN\3. Thesis\BSC\'
+# single ' important to escape the spaces
+
+source 'C:\Users\Matthias\OneDrive - UGent\Documents\DOCUMENTEN\3. Thesis\BSC\bsc\Scripts\activate'
+
+
+### arguments
+# main arguments
+interface='VSC' # either VSC or HPC
+arm_setup='5,0,5,0,0' # number of segments per arm, specify for 5 arms
+reward_type='distance' # choose from 'distance', 'target' or 'light'
+
+es_popsize='500'
+num_generations='70'
+
+sensor_selection='joint_position,joint_actuator_force,segment_contact'
+layers='128,128' # number of nodes in the hidden layers only, specify number of layers in the above variable
+damage='False'
+
+# wandb
+project='Switch_to_VSC' # no spaces
+notes='run_1' # no spaces
+
+# optional arguments: if not provided, default values can be found in the python script itself
+joint_control='position' # either position or torque
+sand_ground_color='False'
+num_physics_steps_per_control_step='10'
+simulation_time='5'
+joint_randomization_noise_scale='0.0'
+light_perlin_noise_scale='4' # is only used in case of 'light' reward type
+target_distance='3' # is only used in case of 'target' reward type
+arm_setup_damage=$arm_setup # Will only be used if the $damage variable is set to 'True'
+
+
+
+# Run the Python script
+python 'Centralized_controller\centr_ctrl_biomimetic.py' \
+  $interface \
+  $arm_setup \
+  $reward_type \
+  $es_popsize \
+  $num_generations \
+  $sensor_selection \
+  $layers \
+  $damage \
+  $project \
+  $notes \
+#   --joint_control $actuator_control \
+#   --sand_ground_color $sand_ground_color \
+#   --num_physics_steps_per_control_step $num_physics_steps_per_control_step \
+#   --simulation_time $simulation_time \
+#   --joint_randomization_nosie_scale $joint_randomization_noise_scale \
+#   --light_perlin_noise_scale $light_perlin_noise_scale \
+#   --target_distance $target_distance \
+#   --arm_setup_damage $arm_setup_damage \
