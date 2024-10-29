9e55a67cb541f5f5062d270f526cbd4d53aafca7
Matthias2807 <matthias.pex@telenet.be>
2024-10-29 10:25:56 +0100

    hpc visualisation shell script

---
+++ run/hpc/visualize_trained_policies_hpc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export MUJOCO_GL="egl"
+export XLA_FLAGS="--xla_gpu_triton_gemm_any=True"
+# export XLA_PYTHON_CLIENT_MEM_FRACTION="0.99" # jax GPU memory preallocation in %
+
+# set to root directory (BSC folder)
+cd "$HOME/bsc/"
+
+# Setup anaconda environment
+conda init
+source ~/.bashrc
+conda activate hope
+
+echo "which python"
+which python
+
+export PYTHONPATH="$HOME/bsc/:$HOME/bsc/bsc_utils/"
+
+
+export POLICY_PARAMS_DIR="./trained_policy_params/b11/"
+export VIDEO_DIR="$VSC_DATA/brittle_star/tmp/"
+export IMAGE_DIR="$VSC_DATA/brittle_star/tmp/"
+export RUN_NAME="b11_r01 distance arms [5, 5, 5, 5, 5] popsize 6912 torque reward _ cost hebbian True"
+
+echo "run started"
+python "./visualize_experiments/visualize_trained_policies.py"
+echo "run finished"
+
+
+
+
